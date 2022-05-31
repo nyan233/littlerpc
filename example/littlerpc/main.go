3e78dcd7f0f520b9439de22b1be6d1633f3f0929
zbh255 <565584326@qq.com>
2022-06-01 00:35:53 +0800

    Update Example

--- example/littlerpc/main.go
+++ example/littlerpc/main.go
@@ -15,6 +15,11 @@ type Hello struct {
 	count int64
 }
 
+type UserJson struct {
+	Name string
+	Id int64
+}
+
 func (h *Hello) Hello(str string,count int64, p []byte) (*int64,error) {
 	atomic.AddInt64(&h.count, count)
 	fmt.Println(str)
@@ -22,6 +27,14 @@ func (h *Hello) Hello(str string,count int64, p []byte) (*int64,error) {
 	fmt.Println(count)
 	var v int64 =  1024 * 1024 * 1024
 	return &v,errors.New("我没有错！")
+}
+
+func (h *Hello) ComplexCall(user UserJson,traces []int64, view map[string]int64) (*UserJson,error){
+	fmt.Println(traces)
+	fmt.Println(view)
+	user.Name = "ComplexCall"
+	user.Id = 1 << 50
+	return &user,littlerpc.Nil
 }
 
 func Server() {
@@ -38,6 +51,7 @@ func Server() {
 
 func Client() {
 	c := littlerpc.NewClient(logger)
+	c.BindFunc(&Hello{})
 	err := c.Dial(":1234")
 	if err != nil {
 		panic(err)
@@ -48,6 +62,17 @@ func Client() {
 	}
 	r1 := rValue[0].(*int64)
 	fmt.Printf("Rpc Server Return Value Pointer : %p -> Value : %d\n",r1,*r1)
+	// 处理复杂的调用
+	rValue, err = c.Call("ComplexCall",UserJson{
+		Name: "hello world",
+		Id:   1024,
+	},[]int64{1 << 10,1 << 11,1 << 12},
+	map[string]int64{"hh":1024,"ll":1234})
+	if err != nil {
+		fmt.Println(err == littlerpc.Nil)
+	}
+	user := rValue[0].(*UserJson)
+	fmt.Println(user)
 }
 
 func main() {
