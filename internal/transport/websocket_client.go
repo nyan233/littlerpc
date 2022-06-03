a292103345d13a141e11dd3a3eb73053cdd2bd7b
zbh255 <565584326@qq.com>
2022-06-03 22:20:55 +0800

    实现Websocket协议作为传输协议

---
+++ internal/transport/websocket_client.go
@@ -0,0 +1,42 @@
+package transport
+
+import (
+	"crypto/tls"
+	"github.com/gorilla/websocket"
+	"net/url"
+)
+
+type WebSocketTransClient struct {
+	conn *websocket.Conn
+}
+
+func NewWebSocketTransClient(tlsC *tls.Config,addr string) *WebSocketTransClient {
+	dialer := websocket.Dialer{
+		TLSClientConfig: tlsC,
+	}
+	u := url.URL{
+		Scheme: "wss",
+		Host: addr,
+		Path: wsUrl,
+	}
+	if tlsC == nil {
+		u.Scheme = "ws"
+	}
+	conn,_, err := dialer.Dial(u.String(),nil)
+	if err != nil {
+		panic(err)
+	}
+	return &WebSocketTransClient{conn: conn}
+}
+
+func (c *WebSocketTransClient) WriteTextMessage(p []byte) error {
+	return c.conn.WriteMessage(websocket.TextMessage,p)
+}
+
+func (c *WebSocketTransClient) RecvMessage() (int,[]byte,error) {
+	return c.conn.ReadMessage()
+}
+
+func (c *WebSocketTransClient) WritePongMessage(p []byte) error {
+	return c.conn.WriteMessage(PongMessage,p)
+}
---
+++ internal/transport/websocket_server.go
@@ -0,0 +1,67 @@
+package transport
+
+import (
+	"github.com/lesismal/llib/std/crypto/tls"
+	"github.com/lesismal/nbio/nbhttp"
+	"github.com/lesismal/nbio/nbhttp/websocket"
+	"net/http"
+)
+
+const (
+	wsUrl = "/LittleRpc-WebSocket"
+)
+
+// WebSocketTransServer 不设置错误处理回调函数则采用默认回调
+// 默认函数中遇到错误就会panic，所以不期望panic的话一定要设置错误处理回调
+type WebSocketTransServer struct {
+	server  *nbhttp.Server
+	onMsg   func(conn *websocket.Conn, messageType websocket.MessageType, bytes []byte)
+	onClose func(conn *websocket.Conn, err error)
+	onOpen  func(conn *websocket.Conn)
+	onErr   func(err error)
+}
+
+func NewWebSocketServer(tlsC *tls.Config, nConfig nbhttp.Config) *WebSocketTransServer {
+	nConfig.TLSConfig = tlsC
+	nConfig.Name = "LittleRpc-Server-WebSocket"
+	nConfig.Network = "tcp"
+	server := nbhttp.NewServer(nConfig)
+	return &WebSocketTransServer{server: server, onErr: func(err error) {
+		panic(err)
+	}}
+}
+
+func (server *WebSocketTransServer) SetOnMessage(onMsg func(conn *websocket.Conn, messageType websocket.MessageType, bytes []byte)) {
+	server.onMsg = onMsg
+}
+
+func (server *WebSocketTransServer) SetOnClose(onClose func(conn *websocket.Conn, err error)) {
+	server.onClose = onClose
+}
+
+func (server *WebSocketTransServer) SetOnOpen(onOpen func(conn *websocket.Conn)) {
+	server.onOpen = onOpen
+}
+
+func (server WebSocketTransServer) SetOnErr(onErr func(err error)) {
+	server.onErr = onErr
+}
+
+func (server *WebSocketTransServer) Start() error {
+	mux := &http.ServeMux{}
+	mux.HandleFunc(wsUrl, func(writer http.ResponseWriter, request *http.Request) {
+		ws := websocket.NewUpgrader()
+		ws.OnMessage(server.onMsg)
+		ws.OnClose(server.onClose)
+		ws.OnOpen(server.onOpen)
+		// 从Http升级到WebSocket
+		conn, err := ws.Upgrade(writer, request, nil)
+		if err != nil {
+			server.onErr(err)
+		}
+		wsConn := conn.(*websocket.Conn)
+		_ = wsConn
+	})
+	server.server.Handler = mux
+	return server.server.Start()
+}
