fec2161de534fc9274d438335f1880f7b6dbefff
zbh255 <565584326@qq.com>
2022-06-22 03:03:38 +0800

    实现了一个什么都不做的日志器来屏蔽全局日志和局部日志来修复CI失败的错误

--- impl/common/logging.go
+++ impl/common/logging.go
@@ -5,12 +5,22 @@ import (
 	"github.com/lesismal/nbio/logging"
 	"github.com/zbh255/bilog"
 	"os"
+	"sync/atomic"
+)
+
+const (
+	OpenLogger int64 = 1 << 10
+	CloseLogger int64 = 1 << 11
 )
 
 var Logger bilog.Logger = bilog.NewLogger(os.Stdout, bilog.PANIC, bilog.WithTimes(),
 	bilog.WithCaller(), bilog.WithLowBuffer(0), bilog.WithTopBuffer(0))
 
+var NilLogger bilog.Logger = new(nilLogger)
+
 var NoCallerLogger bilog.Logger = bilog.NewLogger(os.Stdout, bilog.PANIC, bilog.WithDefault())
+
+var loggerOpen int64
 
 type CustomLogger string
 
@@ -19,21 +29,46 @@ func (c CustomLogger) SetLevel(lvl int) {
 }
 
 func (c CustomLogger) Debug(format string, v ...interface{}) {
+	if !ReadLoggerStatus() {
+		return
+	}
 	NoCallerLogger.Debug(fmt.Sprintf(format, v...))
 }
 
 func (c CustomLogger) Info(format string, v ...interface{}) {
+	if !ReadLoggerStatus() {
+		return
+	}
 	NoCallerLogger.Info(fmt.Sprintf(format, v...))
 }
 
 func (c CustomLogger) Warn(format string, v ...interface{}) {
+	if !ReadLoggerStatus() {
+		return
+	}
 	NoCallerLogger.Trace(fmt.Sprintf(format, v...))
 }
 
 func (c CustomLogger) Error(format string, v ...interface{}) {
+	if !ReadLoggerStatus() {
+		return
+	}
 	NoCallerLogger.ErrorFromString(fmt.Sprintf(format, v...))
+}
+
+func SetOpenLogger(ok bool) {
+	if ok {
+		atomic.StoreInt64(&loggerOpen,OpenLogger)
+	} else {
+		atomic.StoreInt64(&loggerOpen,CloseLogger)
+	}
+}
+
+func ReadLoggerStatus() bool {
+	return atomic.LoadInt64(&loggerOpen) == OpenLogger
 }
 
 func init() {
 	logging.DefaultLogger = new(CustomLogger)
+	SetOpenLogger(true)
 }
---
+++ impl/common/logging_nil.go
@@ -0,0 +1,40 @@
+package common
+
+type nilLogger struct {}
+
+func (n nilLogger) Level() int {
+	return 0
+}
+
+func (n nilLogger) Info(s string) {
+	return
+}
+
+func (n nilLogger) Debug(s string) {
+	return
+}
+
+func (n nilLogger) Trace(s string) {
+	return
+}
+
+func (n nilLogger) ErrorFromErr(e error) {
+	return
+}
+
+func (n nilLogger) ErrorFromString(s string) {
+	return
+}
+
+func (n nilLogger) PanicFromErr(e error) {
+	return
+}
+
+func (n nilLogger) PanicFromString(s string) {
+	return
+}
+
+func (n nilLogger) Flush() {
+	return
+}
+
