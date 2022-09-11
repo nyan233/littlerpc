a20068fb99207d969e2291725696b33c97c42f0a
zbh255 <zhoubh001@foxmail.com>
2022-09-11 13:27:33 +0800

    internal/pool : 优化goroutine pool的性能和增加pool内部的recover
    name                   old time/op    new time/op    delta
    TaskPool/TaskPool-8       192ms ± 0%      15ms ± 0%   ~     (p=1.000 n=1+1)
    TaskPool/NoTaskPool-8    14.5ms ± 0%    14.2ms ± 0%   ~     (p=1.000 n=1+1)
    
    name                   old alloc/op   new alloc/op   delta
    TaskPool/TaskPool-8      1.06kB ± 0%   16.52kB ± 0%   ~     (p=1.000 n=1+1)
    TaskPool/NoTaskPool-8     100kB ± 0%      99kB ± 0%   ~     (p=1.000 n=1+1)
    
    name                   old allocs/op  new allocs/op  delta
    TaskPool/TaskPool-8        10.0 ± 0%    1002.0 ± 0%   ~     (p=1.000 n=1+1)
    TaskPool/NoTaskPool-8     2.01k ± 0%     2.01k ± 0%   ~     (all equal)

--- internal/pool/pool_test.go
+++ internal/pool/pool_test.go
@@ -7,48 +7,34 @@ import (
 	"time"
 )
 
-const nTask = 100000
+const nTask = 1000
 
 func TestTaskPool(t *testing.T) {
 	pool := NewTaskPool(MaxTaskPoolSize, runtime.NumCPU()*4)
 	defer pool.Stop()
 	for i := 0; i < nTask; i++ {
-		pool.Push(func() {
+		_ = pool.Push(func() {
 			time.Sleep(500 * time.Nanosecond)
 		})
 	}
-	pool.Wait()
+	_ = pool.Stop()
 }
 
 func BenchmarkTaskPool(b *testing.B) {
-	pool := NewTaskPool(MaxTaskPoolSize, runtime.NumCPU()*4)
+	pool := NewTaskPool(MaxTaskPoolSize, 1024)
 	defer pool.Stop()
-	countPool := NewCounterPool(1024*16, nil)
 	b.Run("TaskPool", func(b *testing.B) {
 		b.ReportAllocs()
+		var wg sync.WaitGroup
 		for i := 0; i < b.N; i++ {
+			wg.Add(nTask)
 			for j := 0; j < nTask; j++ {
-				pool.Push(func() {
-					time.Sleep(500 * time.Microsecond)
+				_ = pool.Push(func() {
+					time.Sleep(time.Microsecond)
+					wg.Done()
 				})
 			}
-			pool.Wait()
-		}
-	})
-	b.Run("CountPool", func(b *testing.B) {
-		b.ReportAllocs()
-		for i := 0; i < b.N; i++ {
-			for j := 0; j < nTask; j++ {
-				err := countPool.GOGO(func() {
-					time.Sleep(500 * time.Microsecond)
-				})
-				for err != nil {
-					err = countPool.GOGO(func() {
-						time.Sleep(500 * time.Microsecond)
-					})
-				}
-			}
-			pool.Wait()
+			wg.Wait()
 		}
 	})
 	b.Run("NoTaskPool", func(b *testing.B) {
@@ -58,8 +44,8 @@ func BenchmarkTaskPool(b *testing.B) {
 			wg.Add(nTask)
 			for j := 0; j < nTask; j++ {
 				go func() {
-					defer wg.Done()
-					time.Sleep(500 * time.Microsecond)
+					time.Sleep(time.Microsecond)
+					wg.Done()
 				}()
 			}
 			wg.Wait()
--- internal/pool/task_pool.go
+++ internal/pool/task_pool.go
@@ -2,23 +2,28 @@ package pool
 
 import (
 	"context"
-	"runtime"
+	"errors"
 	"sync"
+	"sync/atomic"
 )
 
 const (
-	MaxTaskPoolSize = 1024
+	MaxTaskPoolSize = 1024 * 16
 )
 
 type TaskPool struct {
 	// buf chan
-	tasks chan func()
+	tasks     []chan func()
+	ringIndex int64
+	recoverFn func(poolId int, err interface{})
 	// 用于取消所有goroutine
 	cancelFn context.CancelFunc
 	// 统计关闭的goroutine数量
 	wg *sync.WaitGroup
 	// 池的大小，也即活跃的goroutine数量
 	size int
+	// 关闭的标志
+	closed int64
 }
 
 func NewTaskPool(bufSize, size int) *TaskPool {
@@ -26,7 +31,10 @@ func NewTaskPool(bufSize, size int) *TaskPool {
 	if bufSize > MaxTaskPoolSize {
 		bufSize = MaxTaskPoolSize
 	}
-	pool.tasks = make(chan func(), bufSize)
+	pool.tasks = make([]chan func(), size)
+	for i := 0; i < size; i++ {
+		pool.tasks[i] = make(chan func(), bufSize/size)
+	}
 	pool.size = size
 	pool.wg = &sync.WaitGroup{}
 	pool.wg.Add(size)
@@ -37,12 +45,19 @@ func NewTaskPool(bufSize, size int) *TaskPool {
 func (p *TaskPool) start() {
 	ctx, cancel := context.WithCancel(context.Background())
 	p.cancelFn = cancel
-	for i := 0; i < p.size; i++ {
+	for k, ch := range p.tasks {
+		ich := ch
+		ik := k
 		go func() {
+			defer func() {
+				if err := recover(); err != nil {
+					p.recoverFn(ik, err)
+				}
+			}()
 			defer p.wg.Done()
 			for {
 				select {
-				case fn := <-p.tasks:
+				case fn := <-ich:
 					fn()
 				case <-ctx.Done():
 					return
@@ -52,17 +67,20 @@ func (p *TaskPool) start() {
 	}
 }
 
-func (p *TaskPool) Push(fn func()) {
-	p.tasks <- fn
-}
-
-func (p *TaskPool) Wait() {
-	for !(len(p.tasks) == 0) {
-		runtime.Gosched()
+func (p *TaskPool) Push(fn func()) error {
+	if atomic.LoadInt64(&p.closed) == 1 {
+		return errors.New("pool already closed")
 	}
+	i := int(atomic.AddInt64(&p.ringIndex, 1)-1) % len(p.tasks)
+	p.tasks[i] <- fn
+	return nil
 }
 
-func (p *TaskPool) Stop() {
+func (p *TaskPool) Stop() error {
+	if !atomic.CompareAndSwapInt64(&p.closed, 0, 1) {
+		return errors.New("pool already closed")
+	}
 	p.cancelFn()
 	p.wg.Wait()
+	return nil
 }
