b5d89f3fd68c2ce0dbb8df67b66ac8128a1e9a55
rainstorm <zhoubh001@foxmail.com>
2022-09-19 22:23:44 +0800

    reflect : 实现Slice的+-索引

--- reflect/slice.go
+++ reflect/slice.go
@@ -1,6 +1,7 @@
 package reflect
 
 import (
+	"fmt"
 	"reflect"
 	"unsafe"
 )
@@ -24,4 +25,22 @@ func SliceBackSpace(p interface{}, n uint) interface{} {
 	header.Len += int(n)
 	header.Cap += int(n)
 	return *(*interface{})(unsafe.Pointer(eface))
+}
+
+// SliceIndex 支持负数索引
+func SliceIndex(p interface{}, n int) interface{} {
+	val := reflect.ValueOf(p)
+	if val.Kind() != reflect.Slice {
+		panic("type is not a slice")
+	}
+	// index out range?
+	length := val.Len()
+	if length-1 < n || length < -n {
+		panic(fmt.Sprintf("index out range [%d:%d]", n, length))
+	}
+	if -n > 0 {
+		return val.Index(length - (-n)).Interface()
+	} else {
+		return val.Index(n).Interface()
+	}
 }
