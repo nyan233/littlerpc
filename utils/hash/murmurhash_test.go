28fd5f88346af52f6e3180246fabc5bb0676dc3a
rainstorm <zhoubh001@foxmail.com>
2022-09-19 22:22:27 +0800

    utils/hash : 完善murmurhash的Benchmark

--- utils/hash/murmurhash_test.go
+++ utils/hash/murmurhash_test.go
@@ -1,9 +1,12 @@
 package hash
 
 import (
+	"fmt"
+	"github.com/nyan233/littlerpc/utils/random"
 	"math/rand"
 	"strconv"
 	"testing"
+	"time"
 )
 
 func TestMurmurHash(t *testing.T) {
@@ -14,7 +17,22 @@ func TestMurmurHash(t *testing.T) {
 
 func BenchmarkMurmurHash(b *testing.B) {
 	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
-		Murmurhash3Onx8632([]byte("hello"), 144)
+	for i := 32; i < 2048; i *= 2 {
+		seed := time.Now().Unix()
+		b.Run(fmt.Sprintf("Murmurhash-%d", i), func(b *testing.B) {
+			randStr := genBytes(i)
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				_ = Murmurhash3Onx8632(randStr, uint32(seed))
+			}
+		})
 	}
+}
+
+func genBytes(n int) []byte {
+	bytes := make([]byte, n)
+	for i := 0; i < n; i++ {
+		bytes[i] = byte(random.FastRandN(26)) + 65
+	}
+	return bytes
 }
