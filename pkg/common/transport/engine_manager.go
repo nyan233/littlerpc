64acfe7c33ed31aba57b81d85b49c34ef4c9a9b9
zbh255 <zhoubh001@foxmail.com>
2022-10-10 20:07:37 +0800

    将client&server中的EngineManager内聚到pkg/common/transport中

---
+++ pkg/common/transport/engine_manager.go
@@ -0,0 +1,37 @@
+package transport
+
+import "github.com/nyan233/littlerpc/pkg/container"
+
+var (
+	EngineManager = &engineManager{}
+)
+
+type engineManager struct {
+	serverEngineCollection container.SyncMap118[string, NewServerEngineBuilder]
+	clientEngineCollection container.SyncMap118[string, NewClientEngineBuilder]
+}
+
+func (m *engineManager) RegisterServerEngine(scheme string, builder NewServerEngineBuilder) {
+	m.serverEngineCollection.Store(scheme, builder)
+}
+
+func (m *engineManager) GetServerEngine(scheme string) NewServerEngineBuilder {
+	builder, _ := m.serverEngineCollection.LoadOk(scheme)
+	return builder
+}
+
+func (m *engineManager) RegisterClientEngine(scheme string, builder NewClientEngineBuilder) {
+	m.clientEngineCollection.Store(scheme, builder)
+}
+
+func (m *engineManager) GetClientEngine(scheme string) NewClientEngineBuilder {
+	builder, _ := m.clientEngineCollection.LoadOk(scheme)
+	return builder
+}
+
+func init() {
+	EngineManager.RegisterServerEngine("nbio_tcp", NewNBioTcpServerEngine)
+	EngineManager.RegisterClientEngine("nbio_tcp", NewNBioTcpClientEngine)
+	EngineManager.RegisterServerEngine("nbio_ws", NewNBioWebsocketServerEngine)
+	EngineManager.RegisterClientEngine("nbio_ws", NewNBioWebsocketClientEngine)
+}
