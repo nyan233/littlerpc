a1b018ff844739e7a2859b36369cf13af49a1771
zbh255 <zhoubh001@foxmail.com>
2022-10-14 02:09:43 +0800

    server : init recover print function

--- server/utils.go
+++ server/utils.go
@@ -1,8 +1,11 @@
 package server
 
 import (
+	"fmt"
+	"github.com/nyan233/littlerpc/internal/pool"
 	"github.com/nyan233/littlerpc/pkg/middle/codec"
 	"github.com/nyan233/littlerpc/pkg/middle/packet"
+	"github.com/zbh255/bilog"
 )
 
 func safeIndexCodecWps(s []codec.Wrapper, index int) codec.Wrapper {
@@ -23,4 +26,10 @@ func safeIndexEncoderWps(s []packet.Wrapper, index int) packet.Wrapper {
 		return nil
 	}
 	return s[index]
+}
+
+func serverRecover(logger bilog.Logger) pool.RecoverFunc {
+	return func(poolId int, err interface{}) {
+		logger.ErrorFromString(fmt.Sprintf("poolId : %d -> Panic : %v", poolId, err))
+	}
 }
