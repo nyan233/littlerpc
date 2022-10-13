5dabb973f1dee4ef3f8b68b0c3e66ad818e5305d
zbh255 <zhoubh001@foxmail.com>
2022-10-14 03:15:50 +0800

    example : 重新生成一些文件

---
+++ example/codec/protobuf/Hello_proxy.go
@@ -0,0 +1,42 @@
+/*
+@Generator   : littlerpc-generator
+@CreateTime  : 2022-10-14 02:51:26.9764069 +0800 CST m=+0.006762201
+@Author      : littlerpc-generator
+@Comment     : code is auto generate do not edit
+*/
+package main
+
+import (
+	"github.com/nyan233/littlerpc/client"
+)
+
+type HelloInterface interface {
+	SayHelloToProtoBuf(pb *Student) (*Student, error)
+	SayHelloToJson(jn *Student) (*Student, error)
+}
+
+type HelloProxy struct {
+	*client.Client
+}
+
+func NewHelloProxy(client *client.Client) HelloInterface {
+	proxy := &HelloProxy{}
+	err := client.BindFunc("Hello", proxy)
+	if err != nil {
+		panic(err)
+	}
+	proxy.Client = client
+	return proxy
+}
+
+func (p HelloProxy) SayHelloToProtoBuf(pb *Student) (*Student, error) {
+	rep, err := p.Call("Hello.SayHelloToProtoBuf", pb)
+	r0, _ := rep[0].(*Student)
+	return r0, err
+}
+
+func (p HelloProxy) SayHelloToJson(jn *Student) (*Student, error) {
+	rep, err := p.Call("Hello.SayHelloToJson", jn)
+	r0, _ := rep[0].(*Student)
+	return r0, err
+}
---
+++ example/proxy/FileServer_proxy.go
@@ -0,0 +1,49 @@
+/*
+@Generator   : littlerpc-generator
+@CreateTime  : 2022-10-14 03:10:08.4322692 +0800 CST m=+0.004378601
+@Author      : littlerpc-generator
+@Comment     : code is auto generate do not edit
+*/
+package main
+
+import (
+	"github.com/nyan233/littlerpc/client"
+)
+
+type FileServerInterface interface {
+	SendFile(path string, data []byte) error
+	GetFile(path string) ([]byte, bool, error)
+	OpenSysFile(path string) ([]byte, error)
+}
+
+type FileServerProxy struct {
+	*client.Client
+}
+
+func NewFileServerProxy(client *client.Client) FileServerInterface {
+	proxy := &FileServerProxy{}
+	err := client.BindFunc("FileServer", proxy)
+	if err != nil {
+		panic(err)
+	}
+	proxy.Client = client
+	return proxy
+}
+
+func (p FileServerProxy) SendFile(path string, data []byte) error {
+	_, err := p.Call("FileServer.SendFile", path, data)
+	return err
+}
+
+func (p FileServerProxy) GetFile(path string) ([]byte, bool, error) {
+	rep, err := p.Call("FileServer.GetFile", path)
+	r0, _ := rep[0].([]byte)
+	r1, _ := rep[1].(bool)
+	return r0, r1, err
+}
+
+func (p FileServerProxy) OpenSysFile(path string) ([]byte, error) {
+	rep, err := p.Call("FileServer.OpenSysFile", path)
+	r0, _ := rep[0].([]byte)
+	return r0, err
+}
