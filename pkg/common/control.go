2b88a4665228f1c3b59d71b74819743fec491e7d
zbh255 <zhoubh001@foxmail.com>
2022-10-14 02:17:43 +0800

    pkg/common : 删除不再使用到的MuxReadAll函数, 为MuxWriteAll添加测试

--- pkg/common/control.go
+++ pkg/common/control.go
@@ -3,25 +3,13 @@ package common
 import (
 	"errors"
 	"fmt"
-	"github.com/nyan233/littlerpc/internal/reflect"
 	"github.com/nyan233/littlerpc/pkg/container"
 	"github.com/nyan233/littlerpc/protocol"
 	"io"
-	"sync"
 	"syscall"
 )
 
 const MessageTrace = 0
-
-type WriteLocker interface {
-	sync.Locker
-	io.Writer
-}
-
-type ReadLocker interface {
-	sync.Locker
-	io.Reader
-}
 
 func CheckConnWrite(conn io.Writer, p []byte) error {
 	wn, err := conn.Write(p)
@@ -63,8 +51,10 @@ func ReadControl(c io.Reader, data []byte) error {
 	return nil
 }
 
-// NOTE: Write应该检测属于Nio的中断错误, 因为不确定其它框架中是否实现
-// NOTE: 这些错误的过滤
+// WriteControl
+//
+//	NOTE: Write应该检测属于Nio的中断错误, 因为不确定其它框架中是否实现
+//	NOTE: 这些错误的过滤
 func WriteControl(c io.Writer, data []byte) error {
 	var writeCount int
 	for {
@@ -86,7 +76,7 @@ func WriteControl(c io.Writer, data []byte) error {
 // mmBytes 是供拷贝数据的缓冲区
 // mBytes是要写入的数据
 // startFn是每次循环开始时都会调用的回调函数,它允许你在开始前做一些检查
-func MuxWriteAll(c WriteLocker, muxMsg *protocol.MuxBlock, mmBytes *container.Slice[byte],
+func MuxWriteAll(c io.Writer, muxMsg *protocol.MuxBlock, mmBytes *container.Slice[byte],
 	mBytes []byte, startFn func()) error {
 	if mmBytes == nil {
 		var tmp container.Slice[byte]
@@ -118,142 +108,11 @@ func MuxWriteAll(c WriteLocker, muxMsg *protocol.MuxBlock, mmBytes *container.Sl
 			fmt.Println("Write ", *muxMsg)
 			fmt.Println("Write Raw", string(*mmBytes))
 		}
-		c.Lock()
 		err = WriteControl(c, *mmBytes)
 		if err != nil {
-			c.Unlock()
 			return err
 		}
-		c.Unlock()
 		mBytes = mBytes[sendN:]
 	}
-	return nil
-}
-
-// MuxReadAll checkPoint在Lock之前被执行,如果需要goroutine安全则需要在匿名函数内编写Lock/Unlock
-// oneComplete在完成一次MuxMessage接收时被调用
-// 在Client模式下mmBytes作为读的缓冲,其容量必须 >= protocol.MuxMessageBlockSize
-// Server模式并没有检查点, 因为检查点对于Server来说并没有意义
-// oneComplete回调中被传入的p不应该将其缓存, 如果后续会使用到则应该拷贝它, 因为p大部分的情况下只是mmBytes一部分数据的拷贝
-//
-//	NOTE checkPoint不是必选项,oneComplete回调必须被注册,否则会调用panic
-//	NOTE 不可能出现载荷长度为0的情况,LittleRpc的协议规定了Message的最小长度
-func MuxReadAll(c ReadLocker, mmBytes container.Slice[byte],
-	checkPoint func(c ReadLocker) bool, oneComplete func(mm protocol.MuxBlock) error) error {
-
-	if oneComplete == nil {
-		panic("no pass by oneComplete callback")
-	}
-	if mmBytes.Cap() < protocol.MuxMessageBlockSize {
-		panic("buffer capacity less than protocol.MuxMessageBlockSize")
-	}
-	return muxReadAll(c, mmBytes, checkPoint, oneComplete)
-}
-
-func muxReadAll(c ReadLocker, mmBytes container.Slice[byte],
-	checkPoint func(c ReadLocker) bool, oneComplete func(mm protocol.MuxBlock) error) error {
-
-	c.Lock()
-	bytes := mmBytes
-	for {
-		if checkPoint != nil && !checkPoint(c) {
-			c.Unlock()
-			return nil
-		}
-		var muxMsg protocol.MuxBlock
-		if bytes.Len() < protocol.MuxBlockBaseLen {
-			// Server OnMessage响应的数据包不满足基本长度
-			oldLen := bytes.Len()
-			bytes = bytes[:protocol.MuxBlockBaseLen]
-			err := ReadControl(c, bytes[oldLen:protocol.MuxBlockBaseLen])
-			if err != nil {
-				c.Unlock()
-				return err
-			}
-		}
-		err := protocol.UnmarshalMuxBlock(bytes, &muxMsg)
-		if err != nil {
-			c.Unlock()
-			return err
-		}
-		if MessageTrace > 0 {
-			if mmBytes.Len() > 0 {
-				fmt.Println("Server MuxMsg", muxMsg)
-			} else {
-				fmt.Println("Client MuxMsg", muxMsg)
-			}
-		}
-		if MessageTrace > 0 {
-			if mmBytes.Len() <= 0 {
-				fmt.Printf("Mux:{%d %d %d %d}\n", muxMsg.Flags, muxMsg.StreamId, muxMsg.MsgId, muxMsg.PayloadLength)
-			}
-			if muxMsg.PayloadLength == 0 && mmBytes.Len() > 0 {
-				fmt.Println("Server ReadControl")
-			}
-		}
-		if muxMsg.PayloadLength == 0 {
-			if MessageTrace > 0 {
-				fmt.Println(mmBytes.Len())
-				fmt.Println(muxMsg)
-				after := make([]byte, 160)
-				err := ReadControl(c, after)
-				if err != nil {
-					panic(err)
-				}
-				fmt.Println("After Read", string(after))
-			}
-			c.Unlock()
-			return errors.New("message length is zero")
-		}
-		// 未读出一个完整载荷
-		if muxMsg.Payloads == nil || int(muxMsg.PayloadLength) > muxMsg.Payloads.Len() {
-			if muxMsg.PayloadLength > protocol.MaxPayloadSizeOnMux {
-				bytes = (bytes)[:protocol.MaxPayloadSizeOnMux]
-			} else {
-				if muxMsg.PayloadLength < protocol.MaxPayloadSizeOnMux && bytes.Cap() < int(muxMsg.PayloadLength) {
-					bytes = bytes[:bytes.Cap()]
-					bytes = reflect.SliceBackSpace(bytes, uint(int(muxMsg.PayloadLength)-bytes.Len())).(container.Slice[byte])
-				}
-				bytes = (bytes)[:muxMsg.PayloadLength]
-			}
-			err := ReadControl(c, bytes)
-			if err != nil {
-				c.Unlock()
-				return err
-			}
-			muxMsg.Payloads = bytes
-			if err := oneComplete(muxMsg); err != nil {
-				c.Unlock()
-				return err
-			}
-			if checkPoint == nil {
-				break
-			} else {
-				bytes.Reset()
-			}
-		} else if muxMsg.Payloads != nil && int(muxMsg.PayloadLength) < muxMsg.Payloads.Len() {
-			// 数据中包含下一个载荷,下一个载荷可能是完整包,也可能是半包
-			// 尽快调用oneComplete()因为拷贝数据之后会导致原数据被修改
-			rawPayloads := muxMsg.Payloads
-			muxMsg.Payloads = muxMsg.Payloads[:muxMsg.PayloadLength]
-			if err := oneComplete(muxMsg); err != nil {
-				c.Unlock()
-				return err
-			}
-			bytes = rawPayloads[muxMsg.PayloadLength:]
-		} else {
-			// 仅仅包含一个完整的载荷
-			if err := oneComplete(muxMsg); err != nil {
-				c.Unlock()
-				return err
-			}
-			if checkPoint == nil {
-				break
-			} else {
-				bytes.Reset()
-			}
-		}
-	}
-	c.Unlock()
 	return nil
 }
--- pkg/common/control_test.go
+++ pkg/common/control_test.go
@@ -3,112 +3,49 @@ package common
 import (
 	"bytes"
 	"github.com/nyan233/littlerpc/pkg/container"
-	random2 "github.com/nyan233/littlerpc/pkg/utils/random"
+	"github.com/nyan233/littlerpc/pkg/utils/random"
 	"github.com/nyan233/littlerpc/protocol"
-	"io"
-	"sync"
 	"testing"
-	"unsafe"
 )
 
-func RandomMuxMessage(maxBlock int, maxPayloads int) []byte {
-	nBlock := maxBlock
-	msg := protocol.NewMessage()
-	msg.MsgId = uint64(random2.FastRand())
-	*(*uint32)(unsafe.Pointer(&msg.Scope)) = random2.FastRand()
-	msg.SetMethodName(random2.GenStringOnAscii(100))
-	msg.SetInstanceName(random2.GenStringOnAscii(100))
-	msg.SetMetaData(random2.GenStringOnAscii(30),
-		random2.GenStringOnAscii(34))
-	msg.Payloads = random2.GenBytesOnAscii(uint32(maxPayloads))
-	// generate payloadLayout
-	for i := 0; i < int(random2.FastRandN(10)); i++ {
-		msg.PayloadLayout.AppendS(random2.FastRand())
-	}
-	msg.PayloadLength = uint32(msg.GetLength())
-	var msgBytes []byte
-	protocol.MarshalMessage(msg, (*container.Slice[byte])(&msgBytes))
-	muxMsg := protocol.MuxBlock{
-		Flags:         protocol.MuxEnabled,
-		StreamId:      random2.FastRand(),
-		MsgId:         msg.MsgId,
-		PayloadLength: 0,
-		Payloads:      nil,
-	}
-	var muxMsgBytes []byte
-	oldLen := len(msgBytes)
-	for i := 0; i < nBlock; i++ {
-		split := oldLen / nBlock
-		if i == 0 && split < protocol.MessageBaseLen {
-			split = protocol.MessageBaseLen
+func TestWriteControl(t *testing.T) {
+	w := bytes.NewBuffer(nil)
+	copyBuffer := make([]byte, 0, 4096)
+	randMsg := protocol.NewMessage()
+	randMsg.SetEncoderType(1)
+	randMsg.SetInstanceName(random.GenStringOnAscii(1000))
+	randMsg.SetMethodName(random.GenStringOnAscii(1000))
+	randMsg.MetaData.Store(protocol.ErrorCode, "200")
+	randMsg.MetaData.Store(protocol.ErrorMessage, "message")
+	randMsg.AppendPayloads(random.GenBytesOnAscii(20000))
+	randMsg.AppendPayloads(random.GenBytesOnAscii(30000))
+	var b []byte
+	protocol.MarshalMessage(randMsg, (*container.Slice[byte])(&b))
+	nBlock := len(b) / (protocol.MuxMessageBlockSize - protocol.MuxBlockBaseLen)
+	mod := len(b) % (protocol.MuxMessageBlockSize - protocol.MuxBlockBaseLen)
+	var pointer int
+	one := true
+	err := MuxWriteAll(w, &protocol.MuxBlock{
+		Flags:    protocol.MuxEnabled,
+		StreamId: random.FastRand(),
+		MsgId:    uint64(random.FastRand()),
+	}, (*container.Slice[byte])(&copyBuffer), b, func() {
+		if one {
+			one = false
+			return
 		}
-		muxMsg.PayloadLength = uint16(split)
-		muxMsg.Payloads = msgBytes[:split]
-		tmpMuxMsgBytes := make([]byte, 0)
-		err := protocol.MarshalMuxBlock(&muxMsg, (*container.Slice[byte])(&tmpMuxMsgBytes))
-		if err != nil {
-			panic(err)
+		if pointer > nBlock {
+			if w.Len() != mod+protocol.MuxBlockBaseLen {
+				t.Fatal("mux write bytes no equal")
+			}
+			w.Reset()
+			return
 		}
-		muxMsgBytes = append(muxMsgBytes, tmpMuxMsgBytes...)
-		msgBytes = msgBytes[split:]
-	}
-	return muxMsgBytes
-}
-
-func TestControl(t *testing.T) {
-	type readLock struct {
-		sync.Mutex
-		io.Reader
-	}
-	completeMsgs := RandomMuxMessage(70, 4096)
-	for len(completeMsgs) < protocol.MuxMessageBlockSize {
-		completeMsgs = append(completeMsgs, RandomMuxMessage(2, 4096)...)
-	}
-	rawBytes := RandomMuxMessage(20, 8192)
-	completeMsgs = append(completeMsgs, rawBytes[:5]...)
-	rawBytes = rawBytes[5:]
-	rl := &readLock{
-		Mutex:  sync.Mutex{},
-		Reader: bytes.NewReader(rawBytes),
-	}
-	unmarshalMap := make(map[uint64][]byte)
-	completeMap := make(map[uint64][]byte)
-	err := MuxReadAll(rl, completeMsgs, nil, func(mm protocol.MuxBlock) bool {
-		// 第一次存储
-		if p, ok := unmarshalMap[mm.MsgId]; !ok {
-			var baseMsg protocol.Message
-			err := protocol.UnmarshalMessageOnMux(mm.Payloads, &baseMsg)
-			if err != nil {
-				t.Fatal(err)
-			}
-			p = make([]byte, 0, baseMsg.PayloadLength)
-			p = append(p, mm.Payloads...)
-			if len(p) == cap(p) {
-				completeMap[mm.MsgId] = p
-			} else {
-				unmarshalMap[mm.MsgId] = p
-			}
-		} else {
-			p = append(p, mm.Payloads...)
-			if len(p) == cap(p) {
-				delete(unmarshalMap, mm.MsgId)
-				completeMap[mm.MsgId] = p
-			} else {
-				unmarshalMap[mm.MsgId] = p
-			}
+		if w.Len() != protocol.MuxMessageBlockSize {
+			t.Fatal("mux write bytes no equal")
 		}
-		return true
+		w.Reset()
 	})
-	if len(unmarshalMap) == 0 {
-		t.Fatal("unmarshalMap is not equal zero")
-	}
-	for _, v := range completeMap {
-		msg := protocol.NewMessage()
-		err := protocol.UnmarshalMessage(v, msg)
-		if err != nil {
-			t.Fatal(err)
-		}
-	}
 	if err != nil {
 		t.Fatal(err)
 	}
