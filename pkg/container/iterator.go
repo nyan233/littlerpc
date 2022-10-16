c6182652d84290f0ee788ea3eb32eddcd681dec4
zbh255 <zhoubh001@foxmail.com>
2022-10-16 18:45:31 +0800

    container : iterator增加可以查看长度和重置current pointer的功能

--- pkg/container/iterator.go
+++ pkg/container/iterator.go
@@ -3,25 +3,38 @@ package container
 // Iterator 是一个迭代器, 它能按照你自己设置的规则返回每一次的数据
 // 它的实现不是goroutine安全的
 type Iterator[Elem any] struct {
-	iterate func() (Elem, bool)
-	isEnd   bool
+	iterate func(current int) Elem
+	reset   func()
+	current int
+	tail    int
 }
 
-func NewIterator[Elem any](iterate func() (Elem, bool)) *Iterator[Elem] {
+func NewIterator[Elem any](tail int, iterate func(current int) Elem, reset func()) *Iterator[Elem] {
 	return &Iterator[Elem]{
 		iterate: iterate,
+		tail:    tail,
+		reset:   reset,
 	}
 }
 
 func (iter *Iterator[Elem]) Next() bool {
-	return !iter.isEnd
+	return iter.current < iter.tail
+}
+
+func (iter *Iterator[Elem]) Tail() int {
+	return iter.tail
 }
 
 func (iter *Iterator[Elem]) Take() Elem {
-	if iter.isEnd {
+	if !iter.Next() {
 		return *new(Elem)
 	}
-	elem, end := iter.iterate()
-	iter.isEnd = end
-	return elem
+	e := iter.iterate(iter.current)
+	iter.current++
+	return e
+}
+
+func (iter *Iterator[Elem]) Reset() {
+	iter.current = 0
+	iter.reset()
 }
--- pkg/container/iterator_test.go
+++ pkg/container/iterator_test.go
@@ -4,18 +4,19 @@ import "testing"
 
 func TestIterator(t *testing.T) {
 	elems := []int{10, 18, 20, 40, 58, 68}
-	i := -1
-	iter := NewIterator[int](func() (int, bool) {
-		i++
-		if i == len(elems)-1 {
-			return elems[i], true
-		}
-		return elems[i], false
+	iter := NewIterator[int](len(elems), func(current int) int {
+		return elems[current]
+	}, func() {
+		return
 	})
 	for range elems {
 		iter.Take()
 	}
 	if iter.Next() != false {
 		t.Error("Iterator no end")
+	}
+	iter.Reset()
+	if iter.Next() != true {
+		t.Error("Iterator no reset")
 	}
 }
