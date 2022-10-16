408c4a38b25a5a960e3e82f147dda17feceedb2b
zbh255 <zhoubh001@foxmail.com>
2022-10-16 19:45:20 +0800

    pkg/common : add test case on CheckCoderType()

--- pkg/common/check_test.go
+++ pkg/common/check_test.go
@@ -1,1 +1,30 @@
 package common
+
+import (
+	"github.com/nyan233/littlerpc/pkg/middle/codec"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCheckCoderType(t *testing.T) {
+	_, err := CheckCoderType(&codec.JsonCodec{}, nil, nil)
+	if err == nil {
+		t.Fatal("error equal nil")
+	}
+	bytes := []byte("{\"hello\":\"123\",\"dd\":\"456\"}")
+	var testData map[string]string
+	comparaData := map[string]string{
+		"hello": "123",
+		"dd":    "456",
+	}
+	uTestData, err := CheckCoderType(&codec.JsonCodec{}, bytes, testData)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, uTestData, comparaData)
+	uTestData, err = CheckCoderType(&codec.JsonCodec{}, bytes, &testData)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, uTestData, &comparaData)
+}
