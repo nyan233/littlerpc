768501472c0b92b156362d3e93dddfe0b1283cb1
zbh255 <zhoubh001@foxmail.com>
2022-11-03 00:51:44 +0800

    pkg/utils/control : 删除一些使用不到的代码, 新增ReadControl&WriteControl的测试

--- pkg/utils/control/control.go
+++ pkg/utils/control/control.go
@@ -1,33 +1,9 @@
 package control
 
 import (
-	"errors"
-	"fmt"
-	"github.com/nyan233/littlerpc/pkg/container"
-	"github.com/nyan233/littlerpc/protocol/mux"
 	"io"
 	"syscall"
 )
-
-const MessageTrace = 0
-
-func CheckConnWrite(conn io.Writer, p []byte) error {
-	wn, err := conn.Write(p)
-	if err != nil {
-		return err
-	}
-	if wn != len(p) {
-		return errors.New("write bytes not equal")
-	}
-	return nil
-}
-
-func CheckConnRead(n int, err error) error {
-	if err != nil {
-		return err
-	}
-	return nil
-}
 
 // ReadControl
 //
@@ -68,51 +44,6 @@ func WriteControl(c io.Writer, data []byte) error {
 		if writeCount == len(data) {
 			break
 		}
-	}
-	return nil
-}
-
-// MuxWriteAll muxMsg是预定义的Mux载荷
-// mmBytes 是供拷贝数据的缓冲区
-// mBytes是要写入的数据
-// startFn是每次循环开始时都会调用的回调函数,它允许你在开始前做一些检查
-func MuxWriteAll(c io.Writer, muxMsg *mux.Block, mmBytes *container.Slice[byte],
-	mBytes []byte, startFn func()) error {
-	if mmBytes == nil {
-		var tmp container.Slice[byte]
-		if len(mBytes) <= mux.MaxBlockSize {
-			tmp = make([]byte, 0, len(mBytes))
-		} else {
-			tmp = make([]byte, mux.MaxBlockSize)
-		}
-		mmBytes = &tmp
-	}
-	for len(mBytes) > 0 {
-		if startFn != nil {
-			startFn()
-		}
-		var sendN int
-		if len(mBytes) < mux.MaxPayloadSizeOnMux {
-			sendN = len(mBytes)
-		} else {
-			sendN = mux.MaxPayloadSizeOnMux
-		}
-		mmBytes.Reset()
-		muxMsg.Payloads = mBytes[:sendN]
-		muxMsg.PayloadLength = uint16(sendN)
-		err := mux.Marshal(muxMsg, mmBytes)
-		if err != nil {
-			return err
-		}
-		if MessageTrace > 0 {
-			fmt.Println("Write ", *muxMsg)
-			fmt.Println("Write Raw", string(*mmBytes))
-		}
-		err = WriteControl(c, *mmBytes)
-		if err != nil {
-			return err
-		}
-		mBytes = mBytes[sendN:]
 	}
 	return nil
 }
--- pkg/utils/control/control_test.go
+++ pkg/utils/control/control_test.go
@@ -1,53 +1,88 @@
 package control
 
 import (
-	"bytes"
-	"github.com/nyan233/littlerpc/pkg/container"
 	"github.com/nyan233/littlerpc/pkg/utils/random"
-	"github.com/nyan233/littlerpc/protocol/message"
-	"github.com/nyan233/littlerpc/protocol/mux"
+	"github.com/stretchr/testify/assert"
+	"syscall"
 	"testing"
 )
 
-func TestWriteControl(t *testing.T) {
-	w := bytes.NewBuffer(nil)
-	copyBuffer := make([]byte, 0, 4096)
-	randMsg := message.New()
-	randMsg.SetEncoderType(1)
-	randMsg.SetInstanceName(random.GenStringOnAscii(1000))
-	randMsg.SetMethodName(random.GenStringOnAscii(1000))
-	randMsg.MetaData.Store(message.ErrorCode, "200")
-	randMsg.MetaData.Store(message.ErrorMessage, "message")
-	randMsg.AppendPayloads(random.GenBytesOnAscii(20000))
-	randMsg.AppendPayloads(random.GenBytesOnAscii(30000))
-	var b []byte
-	message.Marshal(randMsg, (*container.Slice[byte])(&b))
-	nBlock := len(b) / (mux.MaxBlockSize - mux.BlockBaseLen)
-	mod := len(b) % (mux.MaxBlockSize - mux.BlockBaseLen)
-	var pointer int
-	one := true
-	err := MuxWriteAll(w, &mux.Block{
-		Flags:    mux.Enabled,
-		StreamId: random.FastRand(),
-		MsgId:    uint64(random.FastRand()),
-	}, (*container.Slice[byte])(&copyBuffer), b, func() {
-		if one {
-			one = false
-			return
-		}
-		if pointer > nBlock {
-			if w.Len() != mod+mux.BlockBaseLen {
-				t.Fatal("mux write bytes no equal")
-			}
-			w.Reset()
-			return
-		}
-		if w.Len() != mux.MaxBlockSize {
-			t.Fatal("mux write bytes no equal")
-		}
-		w.Reset()
-	})
-	if err != nil {
-		t.Fatal(err)
+type testInter struct {
+	clickInterval  int
+	oneCompleteNum int
+	throw          bool
+	callCount      int
+}
+
+func (t *testInter) Read(p []byte) (n int, err error) {
+	defer func() {
+		t.callCount++
+	}()
+	var readN int
+	if len(p) <= t.oneCompleteNum {
+		readN = len(p)
+	} else {
+		readN = t.oneCompleteNum
 	}
+	if t.callCount == t.clickInterval {
+		t.callCount = 0
+		if t.throw {
+			return readN, syscall.EINPROGRESS
+		}
+		switch random.FastRandN(3) {
+		case 0:
+			return readN, syscall.EAGAIN
+		case 1:
+			return readN, syscall.EINTR
+		case 2:
+			return readN, syscall.EWOULDBLOCK
+		}
+	}
+	return readN, nil
+}
+
+func (t *testInter) Write(p []byte) (n int, err error) {
+	defer func() {
+		t.callCount++
+	}()
+	var writeN int
+	if len(p) <= t.oneCompleteNum {
+		writeN = len(p)
+	} else {
+		writeN = t.oneCompleteNum
+	}
+	if t.callCount == t.clickInterval {
+		t.callCount = 0
+		if t.throw {
+			return writeN, syscall.EINPROGRESS
+		}
+		switch random.FastRandN(3) {
+		case 0:
+			return writeN, syscall.EAGAIN
+		case 1:
+			return writeN, syscall.EINTR
+		case 2:
+			return writeN, syscall.EWOULDBLOCK
+		}
+	}
+	return writeN, nil
+}
+
+func TestWriteControl(t *testing.T) {
+	const (
+		ClickInterval  = 10
+		OneCompleteNum = 100
+	)
+	bytes := make([]byte, 1024*1024)
+	inter := &testInter{
+		clickInterval:  ClickInterval,
+		oneCompleteNum: OneCompleteNum,
+	}
+	assert.Equal(t, ReadControl(inter, bytes), nil)
+	inter.callCount = 0
+	assert.Equal(t, WriteControl(inter, bytes), nil)
+	inter.callCount = 0
+	inter.throw = true
+	assert.NotEqual(t, ReadControl(inter, bytes), nil)
+	assert.NotEqual(t, WriteControl(inter, bytes), nil)
 }
