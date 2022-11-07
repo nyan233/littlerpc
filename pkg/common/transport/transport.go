da988969e345b8ea0eadd614051c43ee0a330cbe
zbh255 <zhoubh001@foxmail.com>
2022-11-07 21:09:28 +0800

    pkg/common/transport : add comment

--- pkg/common/transport/transport.go
+++ pkg/common/transport/transport.go
@@ -21,14 +21,12 @@ type ClientEngine interface {
 	NewConn(NetworkClientConfig) (ConnAdapter, error)
 }
 
-// ConnAdapter TODO 定义OnErr的定义
+// ConnAdapter
 // 这个接口定义的实现应该是线程安全的, 可以安全地被多个goroutine共享
 // 而且其指针不应该随便变动, 至少在OnClose()完成调用之前不可以变动
+// 接口中的方法应该是Sync style, 即方法执行完成后所有任务均已完成, 比如Read()为非
+// 阻塞接口的话则不满足要求, 需要实现传输层的框架提供一个Sync style的封装, 否则则会串包
 type ConnAdapter interface {
-	// Read Read/Write如果使用了Nio, 那么就不应该把
-	// syscall.EAGAIN/syscall.EINTR 这种错误往上传递, 应该返回真正的错误
-	Read(b []byte) (n int, err error)
-	Write(b []byte) (n int, err error)
 	// Close 不管因为何种原因导致了连接被关闭, ServerTransportBuilder设置的OnClose
 	// 应该被调用, 从而让LittleRpc能够清理残余数据
 	Close() error
