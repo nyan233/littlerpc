02e2b8515364cd1414bcbe9eadfc4928a6cd1492
zbh255 <zhoubh001@foxmail.com>
2022-11-18 21:02:20 +0800

    protocol/error : 在Message中instance&method的概念被Service取代了, 更新老的error code protocol/error : 更新error code to string的映射

--- protocol/error/error_code.go
+++ protocol/error/error_code.go
@@ -26,17 +26,17 @@ func (c Code) MarshalJSON() ([]byte, error) {
 const (
 	Success               = 200  // 成功返回
 	Unknown               = 730  // 用户过程返回了错误,但不是LittleRpc可以识别的错误
-	MethodNoRegister      = 750  // 需要调用的方法未被注册
-	InstanceNoRegister    = 770  // 需要调用的实例未被注册
+	ServiceNotFound       = 750  // 需要调用的服务不存在
 	MessageDecodingFailed = 780  // 载荷消息解码失败
 	MessageEncodingFailed = 1060 // 载荷消息编码失败
 	ServerError           = 690  // 服务器的其它错误
 	ClientError           = 580  // 客户端产生的错误
-	CallArgsTypeErr       = 1030 // 过程的调用参数类型错误
-	CodecMarshalErr       = 1050 // Codec在序列化数据时出错
-	ConnectionErr         = 1070 // 连接错误
-	ContextNotFound       = 1080 // 要取消的context不存在
-	UnsafeOption          = 2060 // 不安全的选项, 通常在服务器需要的东西没有准备好时触发
+	// CallArgsTypeErr TODO: 计划删除, v0.2.0时代的遗留产物
+	CallArgsTypeErr = 1030 // 过程的调用参数类型错误
+	CodecMarshalErr = 1050 // Codec在序列化数据时出错
+	ConnectionErr   = 1070 // 连接错误
+	ContextNotFound = 1080 // 要取消的context不存在
+	UnsafeOption    = 2060 // 不安全的选项, 通常在服务器需要的东西没有准备好时触发
 )
 
 // NOTE: 不要尝试修改这个表，这个表不应该在运行时被改变或者被使用到
@@ -44,8 +44,7 @@ const (
 var mappingStr = map[Code]string{
 	Success:               "\"Success\"",
 	Unknown:               "\"Unknown\"",
-	MethodNoRegister:      "\"MethodNoRegister\"",
-	InstanceNoRegister:    "\"InstanceNoRegister\"",
+	ServiceNotFound:       "\"ServiceNotFound\"",
 	MessageDecodingFailed: "\"MessageDecodingFailed\"",
 	MessageEncodingFailed: "\"MessageEncodingFailed\"",
 	ServerError:           "\"ServerError\"",
