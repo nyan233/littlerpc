2cf082f899142b7939063ffe0512e5aa52e7e541
zbh255 <zhoubh001@foxmail.com>
2022-11-23 23:54:49 +0800

    pkg/common : 修复JsonErrorHandler()将mores当成单个参数传递给下次函数的Bug

--- pkg/common/err_hadler.go
+++ pkg/common/err_hadler.go
@@ -9,9 +9,9 @@ var DefaultErrHandler = &JsonErrorHandler{}
 type JsonErrorHandler struct{}
 
 func (j JsonErrorHandler) LNewErrorDesc(code int, message string, mores ...interface{}) perror.LErrorDesc {
-	return perror.LNewStdError(code, message, mores)
+	return perror.LNewStdError(code, message, mores...)
 }
 
 func (j JsonErrorHandler) LWarpErrorDesc(desc perror.LErrorDesc, mores ...interface{}) perror.LErrorDesc {
-	return perror.LWarpStdError(desc, mores)
+	return perror.LWarpStdError(desc, mores...)
 }
