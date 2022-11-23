540b0ba17a24eb2086043ef8e25043ae67378220
zbh255 <zhoubh001@foxmail.com>
2022-11-23 23:36:37 +0800

    client : CallOption pre release

--- client/call_option.go
+++ client/call_option.go
@@ -12,40 +12,50 @@ type callConfig struct {
 	Packer packer.Packer
 }
 
-type CallOption func(*callConfig)
+type CallOption func(cc *callConfig)
 
 func WithCallCodec(scheme string) CallOption {
-	return func(c *callConfig) {
-		c.Codec = codec.Get(scheme)
+	return func(cc *callConfig) {
+		cc.Codec = codec.Get(scheme)
 	}
 }
 
 func WithCallWriter(writer msgwriter.Writer) CallOption {
-	return func(c *callConfig) {
-		c.Writer = writer
+	return func(cc *callConfig) {
+		cc.Writer = writer
 	}
 }
 
 func WithCallLRPCNoMuxWriter() CallOption {
-	return func(c *callConfig) {
-		c.Writer = msgwriter.NewLRPCNoMux()
+	return func(cc *callConfig) {
+		cc.Writer = msgwriter.NewLRPCNoMux()
 	}
 }
 
 func WithCallLRPCMuxWriter() CallOption {
-	return func(c *callConfig) {
-		c.Writer = msgwriter.NewLRPCMux()
+	return func(cc *callConfig) {
+		cc.Writer = msgwriter.NewLRPCMux()
 	}
 }
 
 func WithCallJsonRpc2Writer() CallOption {
-	return func(c *callConfig) {
-		c.Writer = msgwriter.NewJsonRPC2()
+	return func(cc *callConfig) {
+		cc.Writer = msgwriter.NewJsonRPC2()
 	}
 }
 
 func WithCallPacker(scheme string) CallOption {
-	return func(c *callConfig) {
-		c.Packer = packer.Get(scheme)
+	return func(cc *callConfig) {
+		cc.Packer = packer.Get(scheme)
+	}
+}
+
+func WithCallJsonRpc2() CallOption {
+	return func(cc *callConfig) {
+		var c *Config
+		WithJsonRpc2()(c)
+		cc.Codec = c.Codec
+		cc.Writer = c.Writer
+		cc.Packer = c.Packer
 	}
 }
