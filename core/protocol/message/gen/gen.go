110c48ce806bf7822dd88abb716ff148891e9f6a
zbh255 <zhoubh001@foxmail.com>
2022-12-21 00:13:11 +0800

    core/protocol/message/gen : 增加生成Mux消息类型的bytes数据

--- core/protocol/message/gen/gen.go
+++ core/protocol/message/gen/gen.go
@@ -3,6 +3,7 @@ package gen
 import (
 	"github.com/nyan233/littlerpc/core/container"
 	message2 "github.com/nyan233/littlerpc/core/protocol/message"
+	"github.com/nyan233/littlerpc/core/protocol/message/mux"
 	"github.com/nyan233/littlerpc/core/utils/random"
 )
 
@@ -36,4 +37,35 @@ func NoMuxToBytes(level int) []byte {
 		return nil
 	}
 	return bytes
+}
+
+func MuxToBytes(level int) []byte {
+	var bytes []byte
+	msg := NoMux(level)
+	err := message2.Marshal(msg, (*container.Slice[byte])(&bytes))
+	if err != nil {
+		return nil
+	}
+	toBytes := make([]byte, 0, len(bytes))
+	for len(bytes) > 0 {
+		var readN int
+		if len(bytes) > mux.MaxPayloadSizeOnMux {
+			readN = mux.MaxPayloadSizeOnMux
+		} else {
+			readN = len(bytes)
+		}
+		muxPayloads := bytes[:readN]
+		block := mux.Block{
+			Flags:         mux.Enabled,
+			StreamId:      0x0bbbb,
+			MsgId:         msg.GetMsgId(),
+			PayloadLength: uint16(readN),
+			Payloads:      muxPayloads,
+		}
+		marshalContainer := make([]byte, 0, readN+mux.BlockBaseLen)
+		mux.Marshal(&block, (*container.Slice[byte])(&marshalContainer))
+		toBytes = append(toBytes, marshalContainer...)
+		bytes = bytes[readN:]
+	}
+	return toBytes
 }
