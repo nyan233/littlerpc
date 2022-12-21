a6f1af757bba40cf775b17208385f7b33a40e20d
zbh255 <zhoubh001@foxmail.com>
2022-12-21 18:43:47 +0800

    core/server : 优化反射服务中获取service参数的接口, 返回的数据更加清晰

--- core/server/reflection.go
+++ core/server/reflection.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"context"
 	"github.com/nyan233/littlerpc/core/common/errorhandler"
 	"github.com/nyan233/littlerpc/core/common/metadata"
 	"github.com/nyan233/littlerpc/core/middle/codec"
@@ -21,9 +22,10 @@ type MethodTable struct {
 	Table      []string
 }
 
+// ArgumentType 参数类型 == map[string]interface{}表示struct,interface{}里的参数也同理 == string时表示其它类型
 type ArgumentType struct {
-	TypeName string      `json:"type_name"`
-	Other    interface{} `json:"other"`
+	Kind string      `json:"kind"`
+	Type interface{} `json:"type"`
 }
 
 // LittleRpcReflection 反射服务
@@ -31,7 +33,7 @@ type LittleRpcReflection struct {
 	rpcServer *Server
 }
 
-func (l *LittleRpcReflection) MethodTable(sourceName string) (*MethodTable, error) {
+func (l *LittleRpcReflection) MethodTable(ctx context.Context, sourceName string) (*MethodTable, error) {
 	source, ok := l.rpcServer.sources.LoadOk(sourceName)
 	if !ok {
 		return nil, errorhandler.ServiceNotfound
@@ -46,7 +48,7 @@ func (l *LittleRpcReflection) MethodTable(sourceName string) (*MethodTable, erro
 	return mt, nil
 }
 
-func (l *LittleRpcReflection) AllInstance() (map[string]string, error) {
+func (l *LittleRpcReflection) AllInstance(ctx context.Context) (map[string]string, error) {
 	mp := make(map[string]string, 4)
 	l.rpcServer.sources.Range(func(key string, value *metadata.Source) bool {
 		mp[key] = value.InstanceType.Name()
@@ -55,7 +57,7 @@ func (l *LittleRpcReflection) AllInstance() (map[string]string, error) {
 	return mp, nil
 }
 
-func (l *LittleRpcReflection) AllCodec() ([]string, error) {
+func (l *LittleRpcReflection) AllCodec(ctx context.Context) ([]string, error) {
 	codecSchemeSet := make([]string, 0, len(codecViewer))
 	for k := range codecViewer {
 		codecSchemeSet = append(codecSchemeSet, k)
@@ -63,7 +65,7 @@ func (l *LittleRpcReflection) AllCodec() ([]string, error) {
 	return codecSchemeSet, nil
 }
 
-func (l *LittleRpcReflection) AllPacker() ([]string, error) {
+func (l *LittleRpcReflection) AllPacker(ctx context.Context) ([]string, error) {
 	packerSchemeSet := make([]string, 0, len(packerViewer))
 	for k := range packerViewer {
 		packerSchemeSet = append(packerSchemeSet, k)
@@ -71,27 +73,88 @@ func (l *LittleRpcReflection) AllPacker() ([]string, error) {
 	return packerSchemeSet, nil
 }
 
-func (l *LittleRpcReflection) MethodArgumentType(serviceName string) ([]*ArgumentType, error) {
+func (l *LittleRpcReflection) MethodArgumentType(ctx context.Context, serviceName string) ([]ArgumentType, error) {
 	service, ok := l.rpcServer.services.LoadOk(serviceName)
 	if !ok {
 		return nil, errorhandler.ServiceNotfound
 	}
-	argDesc := make([]*ArgumentType, 0, 4)
+	argDesc := make([]ArgumentType, 0, 4)
 	typ := service.Value.Type()
-	for i := 1; i < typ.NumIn(); i++ {
+	for i := 0; i < typ.NumIn(); i++ {
 		in := typ.In(i)
-		typDesc := &ArgumentType{}
-		switch in.Kind() {
-		case reflect.Slice:
-			break
-		case reflect.Map:
-			break
-		case reflect.Struct:
-			break
-		default:
-			typDesc.TypeName = in.Name()
-		}
-		argDesc = append(argDesc, typDesc)
+		argDesc = append(argDesc, getArgumentType(in))
 	}
 	return argDesc, nil
+}
+
+func getArgumentType(typ reflect.Type) ArgumentType {
+	switch typ.Kind() {
+	case reflect.Struct:
+		fieldMap := make(map[string]interface{}, typ.NumField())
+		for i := 0; i < typ.NumField(); i++ {
+			field := typ.Field(i)
+			fieldMap[field.Name] = getArgumentType(field.Type)
+		}
+		return ArgumentType{
+			Kind: "struct",
+			Type: fieldMap,
+		}
+	case reflect.Interface:
+		return ArgumentType{
+			Kind: "interface",
+			Type: typ.PkgPath() + "." + typ.Name(),
+		}
+	case reflect.Slice:
+		return ArgumentType{
+			Kind: "array",
+			Type: getTypeName(typ),
+		}
+	case reflect.Map:
+		return ArgumentType{
+			Kind: "map",
+			Type: struct {
+				Key   string
+				Value string
+			}{
+				Key:   getTypeName(typ.Key()),
+				Value: getTypeName(typ.Elem()),
+			},
+		}
+	case reflect.String:
+		return ArgumentType{
+			Kind: "string",
+			Type: typ.Name(),
+		}
+	case reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Int,
+		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return ArgumentType{
+			Kind: "integer",
+			Type: typ.Name(),
+		}
+	case reflect.Pointer:
+		return getArgumentType(typ.Elem())
+	default:
+		return ArgumentType{
+			Kind: "unknown",
+			Type: nil,
+		}
+	}
+}
+
+func getTypeName(typ reflect.Type) string {
+	switch typ.Kind() {
+	case reflect.Slice:
+		return "[]" + getTypeName(typ.Elem())
+	case reflect.Pointer:
+		return "*" + getTypeName(typ.Elem())
+	case reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Int,
+		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return "integer"
+	case reflect.Interface:
+		return typ.PkgPath() + "." + typ.Name()
+	case reflect.Map:
+		return "Map[" + getTypeName(typ.Key()) + "," + getTypeName(typ.Elem()) + "]"
+	default:
+		return typ.Name()
+	}
 }
--- core/server/reflecton_test.go
+++ core/server/reflecton_test.go
@@ -1,9 +1,11 @@
 package server
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"github.com/nyan233/littlerpc/core/common/metadata"
+	"reflect"
 	"testing"
 
 	"github.com/nyan233/littlerpc/core/container"
@@ -21,32 +23,51 @@ func TestReflection(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	table, err := reflection.MethodTable(ReflectionSource)
+	table, err := reflection.MethodTable(context.Background(), ReflectionSource)
 	if err != nil {
 		t.Fatal(err)
 	}
 	t.Log(*table)
-	allInstance, err := reflection.AllInstance()
+	allInstance, err := reflection.AllInstance(context.Background())
 	if err != nil {
 		t.Fatal(err)
 	}
 	t.Log(allInstance)
-	argumentType, err := reflection.MethodArgumentType(fmt.Sprintf("%s.%s", ReflectionSource, "MethodArgumentType"))
+	argumentType, err := reflection.MethodArgumentType(context.Background(), fmt.Sprintf("%s.%s", ReflectionSource, "MethodArgumentType"))
 	if err != nil {
 		t.Fatal(err)
 	}
 	bytes, _ := json.Marshal(argumentType)
 	t.Log(string(bytes))
-	allCodec, err := reflection.AllCodec()
+	allCodec, err := reflection.AllCodec(context.Background())
 	if err != nil {
 		t.Fatal(err)
 	}
 	bytes, _ = json.Marshal(allCodec)
 	t.Log(string(bytes))
-	allPacker, err := reflection.AllPacker()
+	allPacker, err := reflection.AllPacker(context.Background())
 	if err != nil {
 		t.Fatal(err)
 	}
 	bytes, _ = json.Marshal(allPacker)
 	t.Log(string(bytes))
+}
+
+func TestGetArgumentType(t *testing.T) {
+	type FuncArg1 struct {
+		Uid     int64
+		Name    string
+		Comment string
+		Cancel  context.Context
+		Friend  []FuncArg1
+	}
+	type Func1 func(ctx context.Context, a1 *FuncArg1, a2 string, a3 map[string]map[uint64]FuncArg1)
+	typ := reflect.TypeOf((Func1)(nil))
+	for i := 0; i < typ.NumIn(); i++ {
+		bytes, err := json.Marshal(getArgumentType(typ.In(i)))
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Log(string(bytes))
+	}
 }
