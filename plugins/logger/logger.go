5f50f5c2fc7f030dce1f24aefced637e80f6380b
zbh255 <zhoubh001@foxmail.com>
2023-01-04 15:19:35 +0800

    plugins : 模仿Gin实现的日志插件

---
+++ plugins/logger/logger.go
@@ -0,0 +1,147 @@
+package logger
+
+import (
+	"context"
+	"fmt"
+	lContext "github.com/nyan233/littlerpc/core/common/context"
+	"github.com/nyan233/littlerpc/core/middle/plugin"
+	errorCode "github.com/nyan233/littlerpc/core/protocol/error"
+	perror "github.com/nyan233/littlerpc/core/protocol/error"
+	"github.com/nyan233/littlerpc/core/protocol/message"
+	"io"
+	"reflect"
+	"time"
+)
+
+type Logger struct {
+	plugin.AbstractServer
+	w io.Writer
+}
+
+func New(w io.Writer) plugin.ServerPlugin {
+	return &Logger{
+		w: w,
+	}
+}
+
+type timePoint struct{}
+
+type statusCode struct{}
+
+type ctxData struct {
+	Start   int64
+	MsgType string
+}
+
+func (l Logger) Receive4S(pub *plugin.Context, msg *message.Message) perror.LErrorDesc {
+	var msgType string
+	switch msg.GetMsgType() {
+	case message.Call:
+		msgType = "Call"
+	case message.Ping:
+		msgType = "Keepalive"
+	case message.ContextCancel:
+		msgType = "Context-Cancel"
+	}
+	pub.PluginContext = context.WithValue(pub.PluginContext, timePoint{}, ctxData{
+		Start:   time.Now().UnixNano(),
+		MsgType: msgType,
+	})
+	return nil
+}
+
+func (l Logger) Call4S(pub *plugin.Context, args []reflect.Value, err perror.LErrorDesc) perror.LErrorDesc {
+	if err != nil {
+		return l.printLog(pub, nil, err, "Call")
+	}
+	return nil
+}
+
+func (l Logger) AfterCall4S(pub *plugin.Context, args, results []reflect.Value, err perror.LErrorDesc) perror.LErrorDesc {
+	if err != nil {
+		return l.printLog(pub, nil, err, "AfterCall")
+	}
+	if results == nil || len(results) == 0 {
+		return nil
+	}
+	var status int
+	r0 := results[len(results)-1].Interface()
+	if r0 == nil {
+		status = errorCode.Success
+	} else if rErr, ok := r0.(perror.LErrorDesc); ok {
+		status = rErr.Code()
+	} else {
+		status = errorCode.Unknown
+	}
+	pub.PluginContext = context.WithValue(pub.PluginContext, statusCode{}, status)
+	return nil
+}
+
+func (l Logger) Send4S(pub *plugin.Context, msg *message.Message, err perror.LErrorDesc) perror.LErrorDesc {
+	if err != nil {
+		return l.printLog(pub, msg, err, "Send")
+	}
+	return nil
+}
+
+func (l Logger) AfterSend4S(pub *plugin.Context, msg *message.Message, err perror.LErrorDesc) perror.LErrorDesc {
+	return l.printLog(pub, msg, err, "AfterSend")
+}
+
+func (l Logger) printLog(pub *plugin.Context, msg *message.Message, err perror.LErrorDesc, phase string) perror.LErrorDesc {
+	const (
+		KB = 1024
+		MB = KB * 1024
+		GB = MB * 1024
+	)
+	if phase == "" {
+		phase = "Unknown"
+	}
+	data, ok := pub.PluginContext.Value(timePoint{}).(ctxData)
+	if !ok {
+		pub.Logger.Warn("logger error : start time not found")
+		return nil
+	}
+	var status int
+	if err == nil {
+		if ctxStatus, ok := pub.PluginContext.Value(statusCode{}).(int); !ok {
+			status = errorCode.Success
+		} else {
+			status = ctxStatus
+		}
+	} else {
+		status = err.Code()
+	}
+	live := time.Now()
+	service := lContext.CheckService(pub.PluginContext)
+	clientAddr := lContext.CheckRemoteAddr(pub.PluginContext)
+	interval := time.Duration(live.UnixNano() - data.Start)
+	var msgSize uint32
+	if msg != nil {
+		msgSize = msg.GetAndSetLength()
+	}
+	var size string
+	switch {
+	case msgSize < 1024:
+		size = fmt.Sprintf("%.3fB", float64(msgSize))
+	case msgSize/uint32(KB) < 1024:
+		size = fmt.Sprintf("%.3fKB", float64(msgSize)/KB)
+	case msgSize/uint32(MB) < 1024:
+		size = fmt.Sprintf("%.3fMB", float64(msgSize)/MB)
+	case msgSize/uint32(GB) < 1024:
+		size = fmt.Sprintf("%.3fGB", float64(msgSize)/GB)
+	}
+	_, wErr := fmt.Fprintf(l.w, "[LRPC] | %-10s | %s | %7d | %10s | %10s | %-15s | %15s | \"%s\"\n",
+		phase,
+		live.Format("2006/01/02 - 15:04:05"),
+		status,
+		interval,
+		size,
+		clientAddr,
+		data.MsgType,
+		service)
+	if wErr != nil {
+		pub.Logger.Warn("logger write data error : %v", wErr)
+	}
+	return nil
+}
---
+++ plugins/logger/logger_test.go
@@ -0,0 +1,44 @@
+package logger
+
+import (
+	"context"
+	lContext "github.com/nyan233/littlerpc/core/common/context"
+	"github.com/nyan233/littlerpc/core/common/errorhandler"
+	"github.com/nyan233/littlerpc/core/common/logger"
+	"github.com/nyan233/littlerpc/core/middle/plugin"
+	"github.com/nyan233/littlerpc/core/protocol/message"
+	"github.com/nyan233/littlerpc/core/protocol/message/gen"
+	"github.com/stretchr/testify/assert"
+	"net"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestPluginLogger(t *testing.T) {
+	l := New(os.Stdout)
+	pCtx := lContext.WithService(context.Background(), "test/api/v1.Hello")
+	ctx := &plugin.Context{
+		PluginContext: pCtx,
+		Logger:        logger.DefaultLogger,
+		EHandler:      nil,
+	}
+	msg := gen.NoMux(gen.Big)
+	testLogger(t, msg, ctx, l, message.Call)
+	testLogger(t, msg, ctx, l, message.ContextCancel)
+	testLogger(t, msg, ctx, l, message.Ping)
+}
+
+func testLogger(t *testing.T, msg *message.Message, ctx *plugin.Context, l plugin.ServerPlugin, msgType uint8) {
+	msg.SetMsgType(msgType)
+	assert.Nil(t, l.Receive4S(ctx, msg))
+	msg.SetServiceName("")
+	time.Sleep(time.Nanosecond * 100)
+	addr, _ := net.ResolveTCPAddr("tcp", "localhost:52372")
+	ctx.PluginContext = lContext.WithRemoteAddr(ctx.PluginContext, addr)
+	assert.Nil(t, l.AfterSend4S(ctx, msg, nil))
+	newAddr, _ := net.ResolveTCPAddr("tcp", "localhost:325")
+	ctx.PluginContext = lContext.WithRemoteAddr(ctx.PluginContext, newAddr)
+	time.Sleep(time.Nanosecond * 120)
+	assert.Nil(t, l.AfterSend4S(ctx, msg, errorhandler.ContextNotFound))
+}
